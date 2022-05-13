3e731e112b71def146a45c341dfae6ac7037088c
Andrea Cleland <andreaclelan@gmail.com>
2022-05-13 15:15:47 -0700

    created reducer hook for shopping list, with add-item action

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,44 @@
+import { useReducer, useState } from 'react';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  const ACTIONS = {
+    ADD_ITEM: 'add-item',
+  };
+
+  function newItem(name) {
+    return { id: Date.now(), name: name, complete: false };
+  }
+
+  function reducer(list, action) {
+    switch (action.type) {
+      case ACTIONS.ADD_ITEM:
+        return [...list, newItem(action.payload.name)];
+    }
+  }
+
+  const [list, dispatch] = useReducer(reducer, []);
+  const [item, setItem] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_ITEM, payload: { name: item } });
+    setItem('');
+  }
+
+  return (
+    <>
+      <h1>Shopping List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={item}
+          type="text"
+          onChange={(e) => {
+            setItem(e.target.value);
+          }}
+          placeholder="Enter item"
+        />
+        <button>Add item</button>
+      </form>
+    </>
+  );
 }
